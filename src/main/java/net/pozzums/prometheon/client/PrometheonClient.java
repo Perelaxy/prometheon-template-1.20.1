e22ecb9682e2a8c353df23bacb92672c9529767e
Thorbjørn Landberg <tho09045@skole.vestfoldfylke.no>
2026-02-26 11:01:53 +0100

    first commit

---
+++ src/main/java/net/pozzums/prometheon/client/PrometheonClient.java
@@ -0,0 +1,10 @@
+package net.pozzums.prometheon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrometheonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
